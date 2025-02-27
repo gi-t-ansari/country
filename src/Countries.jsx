8c5614bf6033fde89bf2cce3aed1aff4d90b5d6d
Tanzeel Ansari <121967095+gi-t-ansari@users.noreply.github.com>
2025-02-27 23:28:19 +0530

    Update Countries.jsx

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -29,20 +29,20 @@ function Countries() {
 
   useEffect(() => {
     // async function fetchAPI() {}
-    fetch("https://restcountries.com/v3.1/all")
+    fetch("https://xcountries-backend.azurewebsites.net/all")
       .then((response) => response.json())
       .then((data) => {
         setCountries(data);
         console.log(data);
       })
-      .catch((error) => console.error("Error occurred while fetching", error));
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
   return (
     <div style={containerStyle}>
       {countries.map((country) => (
         <div key={country.cca3} style={cardStyle}>
-          <img src={country.flags.png} alt="" style={imageStyle} />
-          <h2>{country.name.common}</h2>
+          <img src={country.flag} alt="" style={country.abbr} />
+          <h2>{country.name}</h2>
         </div>
       ))}
     </div>
